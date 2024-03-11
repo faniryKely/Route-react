1b2dc15dca2bb41b56a4895a13819294e173ea1e
Faniry <faniryrandria08@gmail.com>
2024-03-11 10:58:18 +0300

    feat: create blog list

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,14 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
+    <>
+
     <h1>Home Page</h1>
+    <h2>Blog 1</h2>
+    <h2>Blog 2</h2>
+    <h2>Blog 3</h2>
+
+    </>
+    
   );
 }
